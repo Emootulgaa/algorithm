ade2b2ec0fe48ffa44fdba26238369f943474b76
Emootulgaa <68711572+Emootulgaa@users.noreply.github.com>
2022-01-17 02:41:29 +0800

    Add files via upload

---
+++ bubble_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+ void sort(int b[],int a){
+ 	  for(int i=0; i<a;i++){
+		for(int j=0;j<a-1;j++){ 
+			if(b[j]>b[j+1]){
+				int s = b[j];
+				b[j] = b[j+1];
+				b[j+1] = s;
+			}
+		}
+		 cout<<i+1<<"=";
+		for(int c=0; c<a; c++){
+			cout<<" "<<b[c];
+		}
+		cout<<endl;
+	  }
+	}
+
+ void print(int b[], int a){
+ 	cout<<"array sorting :";
+ 	for(int i=0; i<a; i++)
+ 	 cout<<" "<<b[i];
+ }
+int main(){
+    int arr[]={5,4,3,2,1};
+    int size = sizeof(arr)/sizeof(arr[0]);
+	sort(arr,size);
+	print(arr,size);
+}
